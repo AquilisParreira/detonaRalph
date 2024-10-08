6db713d63cc0568127043095ed6af55ed3d1d04d
AquilisParreira <aquilisparreira2022@gmail.com>
2024-10-07 22:33:23 -0300

    Add files via upload
    Desafio de código. Jogo detona Ralph HTML/CSS/JAVASCRIPT

---
+++ src/scripts/jsRalf.js
@@ -0,0 +1,77 @@
+//*+*+*+*+*+*+CONSTANTES ENGINE+*+*+*+*+*+* 
+const state = {
+   
+    view:{
+        squares: document.querySelectorAll(".square"),enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score")
+    } ,
+
+    values:{
+        timerID: null,
+        countDownTimerId:setInterval(countDown,1000),
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result:0,
+        currentTime: 60,
+    },
+}
+//*+***+*+**+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+
+//+*+*+*+*+*+*+*FUNCOES +*+*+*+*+*+*+*+*+*+*+*+*+*+*
+
+function countDown(){
+    state.values.currentTime --;
+    state.view.timeLeft.textContent =state.values.currentTime;
+
+if(state.values.currentTime <= 0){
+   clearInterval(state.values.countDownTimerId);
+   clearInterval(state.values.timerID);
+   
+    alert("GAME OVER !! O seu resultado foi:" + state.values.result )
+    
+
+}
+}
+
+function playSound(audioName){
+let audio = new Audio(`./src/sound/${audioName}.mp3`)
+audio.play();
+}
+
+function randomSquare()
+{ state.view.squares.forEach ((square)=>{
+       square.classList.remove ("enemy"); 
+    } );
+
+let randomNumber = Math.floor(Math.random() * 9);
+let randomSquare = state.view.squares[randomNumber];
+randomSquare.classList.add ("enemy");
+state.values.hitPosition = randomSquare.id;
+}
+function moveEnemy(){
+state.values.timerID = setInterval(randomSquare,state.values.gameVelocity);
+}
+
+function addListenerHitBox(){
+state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown",()=> {
+        if(square.id === state.values.hitPosition){
+            state.values.result++ ;
+            state.view.score.textContent =state.values.result;
+            state.values.hitPosition = null;
+            playSound("hit");
+        }
+    });
+} );
+ 
+}
+
+
+function init() {
+moveEnemy();
+addListenerHitBox();
+}
+ 
+init();
+ //*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
